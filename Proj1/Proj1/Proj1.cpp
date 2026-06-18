e14e4b1ccdc891c55a985bcfb96487978bfda44c
Abdullatif Alkordi <abdo.n.k.123713@gmail.com>
2026-06-19 01:44:01 +0300

    Add files via upload

---
+++ Proj1/Proj1/Proj1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include "clsDblLinkedList.h"
+
+using namespace std;
+
+int main()
+{
+
+    clsDblLinkedList <int> MydblLinkedList;
+
+    MydblLinkedList.InsertAtBeginning(5);
+    MydblLinkedList.InsertAtBeginning(4);
+    MydblLinkedList.InsertAtBeginning(3);
+    MydblLinkedList.InsertAtBeginning(2);
+    MydblLinkedList.InsertAtBeginning(1);
+
+    cout << "\nLinked List Contenet:\n";
+    MydblLinkedList.PrintList();
+    
+    cout << "\n\nLinked List After Add :\n";
+    MydblLinkedList.InsertAfter(4, 500);
+    MydblLinkedList.PrintList();
+
+
+
+    system("pause>0");
+
+}
